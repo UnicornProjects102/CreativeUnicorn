fa0c24204462b01939b998fb0898f1fc744ba20c
Karolina <karolina.bednarczyk102@gmail.com>
2020-10-28 10:03:03 +0100

    modified contact css

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,6 +1,3 @@
-import Router from "./router.js";
-import routes from "./routes.js";
-
 const menuBtn = document.querySelector(".menu-btn");
 const nav = document.querySelector(".nav");
 const menuNav = document.querySelector(".menu-nav");
@@ -12,37 +9,6 @@ const homeLink = document.querySelector(".homelink");
 const aboutLink = document.querySelector(".aboutlink");
 const projectsLink = document.querySelector(".projectslink");
 const contactLink = document.querySelector(".contactlink");
-
-const router = new Router(routes);
-
-homeLink.addEventListener("click", () => {
-  router.loadRoute("");
-  nav.classList.remove("open");
-  menuNav.classList.remove("open");
-  navItems.forEach((item) => item.classList.remove("open"));
-  showMenu = false;
-});
-aboutLink.addEventListener("click", () => {
-  router.loadRoute("about");
-  nav.classList.remove("open");
-  menuNav.classList.remove("open");
-  navItems.forEach((item) => item.classList.remove("open"));
-  showMenu = false;
-});
-projectsLink.addEventListener("click", () => {
-  router.loadRoute("projects");
-  nav.classList.remove("open");
-  menuNav.classList.remove("open");
-  navItems.forEach((item) => item.classList.remove("open"));
-  showMenu = false;
-});
-contactLink.addEventListener("click", () => {
-  router.loadRoute("contact");
-  nav.classList.remove("open");
-  menuNav.classList.remove("open");
-  navItems.forEach((item) => item.classList.remove("open"));
-  showMenu = false;
-});
 
 document.addEventListener("click", (e) => {
   if (e.target.classList.contains("arrow-icon")) {
